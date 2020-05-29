9f41d2c0c37443c6c05e17dc3c5f21d5648e2064
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-05-29 17:28:09 -0600

    create migration and add columns to database for tracking email confirmations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_220716) do
+ActiveRecord::Schema.define(version: 2020_05_29_231257) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_05_29_220716) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "email_hash"
+    t.boolean "email_confirmed", default: false
+    t.string "confirm_token"
   end
 
 end
