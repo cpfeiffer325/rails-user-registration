0ff5ae1d6b00af858aa219e6fb1bb0061f2897d6
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-05-29 15:44:46 -0600

    create form with routes and migration to register a user with email and password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
+  def index
+    
+  end
 
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+
+    @user.save
+    redirect_to @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email, 
+      :password
+    )
+  end
 end
