422b3fc7b9cebb933c59ba2f8cb2b33a3e55ad15
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-05-30 08:48:05 -0600

    add logic to user controller to send confirmation email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,19 @@ class UsersController < ApplicationController
   end
   
   def create
-    create_user_service = CreateUser.new(user_params)
-    @user = create_user_service.create_user
-    redirect_to @user
+    # create_user_service = CreateUser.new(user_params)
+    # @user = create_user_service.create_user
+
+    @user = User.new(user_params)
+
+    if @user.save
+      UserMailer.registration_confirmation(@user).deliver
+      flash[:success] = "Please check your email to confirm your addres to continue"
+      redirect_to @user
+    else
+      flash[:error] = "Aaaaah, something is missing. Sending you back to register properly"
+      redirect_to 'new'
+    end
   end
 
   private
