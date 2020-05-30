4ada0f4e052ab9f3b19e8f3c0b22bfa9d4ad48a5
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-05-30 08:47:37 -0600

    add confirmation email and token to user route and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  before_create :confirmation_token
+  
+  private
+
+  def confirmation_token
+    if self.confirm_token.blank?
+      self.confirm_token = SecureRandom.urlsafe_base64.to_s
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:index, :new, :create, :show]
+  resources :users, only: [:index, :new, :create, :show] do
+    member do
+      get :confirm_email
+    end
+  end
 end
