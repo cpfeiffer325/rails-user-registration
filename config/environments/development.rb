dbc57d48614ed6e3998b7282b1569d2c7c7df5c1
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-05-30 21:04:21 -0600

    add action mailer configurations in dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,11 +32,19 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.default_url_options = { :host => "cpfeiffer325@gmail.com" }
-  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {   
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :domain         => 'gmail.com',
+    :authentication => :plain,
+    :user_name      => 'cpfeiffer325@gmail.com',
+    :password       => 'abc123'
+  }
   config.action_mailer.perform_caching = false
-  
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
