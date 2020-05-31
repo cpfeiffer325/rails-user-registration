0072bfa59dbc47e3482c0c0881b69c910904fcd9
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-05-30 19:04:13 -0600

    add welcome page with link to register a new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get 'welcome/index'
+
   resources :users, only: [:index, :new, :create, :show] do
     member do
       get '/:token/confirm_email/', :to => "users#confirm_email", as: 'confirm_email'
     end
   end
+  
+  root 'welcome#index'
 end
