495bb626f47ef50fd051282cfdc6c58d8a07b3ef
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-06-01 18:43:53 -0600

    replace welcome controller but generating a new sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+  get 'welcome', to: 'sessions#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'welcome/index'
+  # get 'welcome/index'
 
-  resources :users, only: [:index, :new, :create, :show] do
+  resources :users, only: [:new, :create, :show] do
     member do
       get 'confirm_email'
     end
   end
   
-  root 'welcome#index'
+  root 'sessions#welcome'
 end
