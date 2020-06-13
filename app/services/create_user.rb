9fb4c01ce166922aad219028f244ec269787c4de
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-06-12 22:35:30 -0600

    add mailer and reminder job to create user service

--- app/services/create_user.rb
+++ app/services/create_user.rb
@@ -1,10 +1,18 @@
 class CreateUser
-  def initialize params
+  def initialize params # if working update for @email
     @params = params
   end
 
   def create_user
     email_hash = Digest::MD5.hexdigest(@params[:email])
-    User.create(@params.merge(email_hash: email_hash))
+    user = User.create(@params.merge(email_hash: email_hash))
+    # UserMailer.registration_confirmation(user).deliver
+    # UsersReminderJob.set(wait: 24.hours).perform_later(@user)
   end
+
+  private
+
+  # def email_hash
+  #   Digest::MD5.hexdigest(@params[:email])
+  # end
 end
